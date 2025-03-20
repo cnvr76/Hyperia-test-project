2944118faaf4ffc2148d79a30944947dd02deaf5
killrr <64957160+cnvr76@users.noreply.github.com>
2025-03-20 18:46:51 +0100

    /files: Added async parsing Finished version with optimized scraping and async for dynamic pages

--- Parser.py
+++ Parser.py
@@ -3,14 +3,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 
-import time
+import asyncio
+
 import re
+import random
 from bs4 import BeautifulSoup
 
 from FileWriter import Writer
 from typing import List, Tuple, Dict, Any, Optional
-
-from pprint import pprint
 
 
 class Parser:
@@ -18,22 +18,62 @@ class Parser:
         self.__final_json: str = "responses/parsed_page.json"
         self.__link: str = "https://www.prospektmaschine.de/hypermarkte/"
 
-        self.__options = webdriver.ChromeOptions()
-        self.__options.add_argument("--headless")
-        self.__options.add_argument("--disable-gpu")
-        self.__options.add_argument("--window-size=1920,1080")
+    @property
+    def final_json(self):
+        return self.__final_json
 
-        self.__driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.__options)
-    
-    def send_request(self, load_time_s: int = 2) -> str:
-        self.__driver.get(self.__link)
-        time.sleep(load_time_s)
-        return self.__driver.page_source
+    def get_leftside_menu_shop_urls(self) -> List[Dict[str, str]]:
+        """Returns the list of all the shops inside the side panel with info like url and shop's name"""
+        html: str = self.__send_request_selenium(self.__link, 1)
+        
+        soup: BeautifulSoup = BeautifulSoup(html, 'html.parser')
+        
+        shops: List[Dict[str, str]] = []
+        leftmenu = soup.find("div", attrs={"id": "sidebar"}).find("div", attrs={"class": "box"}) 
+        for li in leftmenu.find_all("li"):
+            # returns the path parameter ("/<smth>/")
+            link: str = li.find("a", href=True).get("href")
+            
+            if not link:
+                continue
+            
+            shops.append({"shop_name": li.text.strip(),"link": f"https://www.prospektmaschine.de{link}"})
+        
+        return shops
 
-    def parse_info(self, html: str, override: bool = True) -> List[Dict[str, str]]:  
+    async def send_request_async(self, link: str, load_time_s: int = 4):
+        """Sends async request with random time interval to avoid oversaturation of the requests"""
+        await asyncio.sleep(random.uniform(2, 5))
+        return await asyncio.to_thread(self.__send_request_selenium, link, load_time_s)
+
+    def __send_request_selenium(self, link: str, load_time_s: int) -> str:
+        """Synchronous function to get html from dynamic websites using selenium"""
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-features=NetworkService")
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+        try:
+            driver.get(link)
+            driver.implicitly_wait(load_time_s)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            return driver.page_source
+        except Exception as e:
+            print(f"Ошибка при загрузке {link}: {e}")
+            return ""
+        finally:
+            driver.quit()
+
+    async def parse_info(self, html: str, shop_name: str) -> List[Dict[str, str]]:  
         soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
 
-        brochures = soup.find_all("div", attrs={"class": "brochure-thumb"})
+        brochures_grid = soup.find("div", attrs={"class": "page-body"}).find("div", attrs={"class": "letaky-grid"})
+        brochures = brochures_grid.find_all("div", attrs={"class": "brochure-thumb"})
 
         brochures_parsed: List[Dict[str, str]] = []
         for brochure in brochures:
@@ -41,7 +81,6 @@ class Parser:
                 description_tag = brochure.find("div", attrs={"class": "letak-description"})
                 title_tag = description_tag.find("strong")
                 dates_tag = description_tag.find("small", attrs={"class": "hidden-sm"})
-                shop_name_tag = description_tag.find("a").find("div", attrs={"class": "grid-logo"}).find("img", alt=True)
                 
                 image_tag = brochure.find("div", attrs={"class": "img-container"}).find("img")
 
@@ -50,13 +89,11 @@ class Parser:
                     valid_from: datetime = parsed_date[0]
                     valid_to: datetime = parsed_date[1]
 
-                shop_name: str = shop_name_tag.get("alt").replace("Logo", "").strip() if shop_name_tag else "Not found"
-
                 info = {
                     "title": title_tag.text if title_tag else "Not found",
                     "thumbnail": image_tag.get("src") or image_tag.get("data-src") if image_tag else "Not found",
                     "shop_name": shop_name,
-                    "valid_from": valid_from.strftime('%m-%d-%Y') if valid_to else "Not found",
+                    "valid_from": valid_from.strftime('%m-%d-%Y') if valid_from else "Not found",
                     "valid_to": valid_to.strftime('%m-%d-%Y') if valid_to else "Not specified",
                     "parsed_time": datetime.now().strftime("%m-%d-%Y %H:%M:%S")
                 }
@@ -64,21 +101,17 @@ class Parser:
             except Exception as e:
                 print(e)
 
-        if override:
-            Writer.write_to_json(brochures_parsed, self.__final_json)
-
         return brochures_parsed
-    
-    def close_connection(self) -> None:
-        self.__driver.quit()
 
     @staticmethod
     def parse_date(date: str) -> Tuple[Optional[datetime]]:
         try:
+            # if date has smth like "von Montag 03.03.2025" inside
             if date.find("-") == -1:
                 clean_date = re.sub(r"[^\d.]", "", date).replace(" ", "")
                 valid_from = datetime.strptime(clean_date, "%d.%m.%Y")
                 return (valid_from, None)
+            # for formats like this "03.03.2025 - 30.03.2025"
             else:
                 date_splitted = date.replace(" ", "").split("-")
                 valid_from = datetime.strptime(date_splitted[0], "%d.%m.%Y")
@@ -93,5 +126,6 @@ class Parser:
 
     @staticmethod
     def log(data: Any) -> None:
+        """Used for outputing big chunk of text into log.txt for more redability"""
         filename: str = "logging/log.txt"
         Writer.write_to_file(f"Log Date: {datetime.now()}\n\n{data}", filename)
--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from Parser import Parser
-from pprint import pprint
-from typing import Dict
+from typing import Dict, List
 from FileWriter import Writer
 
+import asyncio
 
-if __name__ == "__main__":
+BATCH_SIZE = 4
+DELAY_BETWEEN_BATCHES = 5
+
+async def main() -> None:
+    """
+    JSON output is that it has list for every shop (with info for every card)
+    and then list that contains BATCH_SIZE amount of these shops in it
+    (can be flatten with itertools.chain(...) method later if needed)
+    """
     parser: Parser = Parser()
 
-    html: str = parser.send_request(1)
-    parser.close_connection()
+    shop_data: List[Dict[str, str]] = parser.get_leftside_menu_shop_urls()
+    shop_count: int = len(shop_data)
 
-    info: Dict[str, str] = parser.parse_info(html)
+    final_result = []
+    for i in range(0, shop_count, BATCH_SIZE):
+        batch = shop_data[i: i + BATCH_SIZE]
+
+        tasks = [parser.send_request_async(shop["link"]) for shop in batch]
+        responses = await asyncio.gather(*tasks)
+
+        parse_tasks = [parser.parse_info(html, shop["shop_name"]) for html, shop in zip(responses, shop_data)]
+        parsed_results = await asyncio.gather(*parse_tasks)
+
+        final_result.append(parsed_results)
+
+        print(f"\n------Group {i // BATCH_SIZE + 1} from ~{shop_count // BATCH_SIZE} completed! Waiting {DELAY_BETWEEN_BATCHES}s to start another one...")
+        await asyncio.sleep(DELAY_BETWEEN_BATCHES)
 
     
+    Writer.write_to_json(final_result, parser.final_json)
+    print("\n\nParsed succesfully...\n\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
